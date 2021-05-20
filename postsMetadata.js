18b8d3afb3ccea14d5e4a9b1fe75e50a561a94ea
Eric Schweichler <eric.schweichler@gmail.com>
2021-05-19 22:42:41 -0400

    Adding post metada
    So we can check to make sure the post exists and do some automated stuff for the archives later.

---
+++ postsMetadata.js
@@ -0,0 +1,10 @@
+var postsMetadata = [
+	"LookingAt360ActionCamerasForMushhing": {
+		"date" : "12 May 2021",
+		"title" : "Looking at 360° Action Cameras for Mushing"
+	},
+	"RaySmith": {
+		"date" : "4 April 2018",
+		"title" : "Raymond M. Smith (1937-2018)"
+ 	}
+]
