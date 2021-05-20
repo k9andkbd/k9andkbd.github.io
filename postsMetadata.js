c1076fb7e8c5a86d426e693b2e9f02864a9c333c
Eric Schweichler <eric.schweichler@gmail.com>
2021-05-19 22:48:50 -0400

    Update postsMetadata.js

--- postsMetadata.js
+++ postsMetadata.js
@@ -1,10 +1,14 @@
 var postsMetadata = {
-	"LookingAt360ActionCamerasForMushhing": {
+	"LookingAt360ActionCamerasForMushing": {
 		"date": "12 May 2021",
 		"title": "Looking at 360° Action Cameras for Mushing"
 	},
 	"RaySmith": {
 		"date": "4 April 2018",
 		"title": "Raymond M. Smith (1937-2018)"
+	},
+	"Archives": {
+		"date": "",
+		"title": "Archives"
 	}
 }
